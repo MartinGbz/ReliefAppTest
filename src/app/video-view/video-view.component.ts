24da58d4f8645ddbb989ef3ff486aad9e64b4b83
MartinGbz <martin.grabarz@outlook.fr>
2021-04-08 16:47:41 +0200

    video url (not embed) works

--- src/app/video-view/video-view.component.ts
+++ src/app/video-view/video-view.component.ts
@@ -17,8 +17,22 @@ export class VideoViewComponent implements OnInit {
     console.log('SET URL');
     console.log('this.serv.servSearchValue = ' + this.serv.servSearchValue);
     // this.URLvideo = this.serv.servSearchValue;
-    document.getElementById('video').setAttribute('src', this.serv.servSearchValue);
+    document.getElementById('video').setAttribute('src', this.getEmbedURL(this.serv.servSearchValue));
     console.log(document.getElementById('video'));
+  }
+
+  getID(url): string {
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+    const match = url.match(regExp);
+
+    return (match && match[2].length === 11)
+      ? match[2]
+      : null;
+  }
+
+  getEmbedURL(url): string {
+    const id = this.getID(url);
+    return 'https://www.youtube.com/embed/' + id;
   }
 
   ngOnInit(): void {
