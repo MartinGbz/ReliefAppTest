4dc2f3d04ef75c6e3bb1897b0a86bcb8fe506696
MartinGbz <martin.grabarz@outlook.fr>
2021-04-12 17:53:10 +0200

    video-view component code cleaning

--- src/app/video-view/video-view.component.ts
+++ src/app/video-view/video-view.component.ts
@@ -15,12 +15,6 @@ export class VideoViewComponent implements OnInit {
    * @param from : 'search' => function called by search button click / 'select' => function called by urlSelection
    */
   setUrl(from): void{
-    console.log('SET URL');
-    console.log('this.serv.oldSlectedItem = ' + this.serv1Service.oldSelectedUrl);
-    console.log('this.serv.selectedItem = ' + this.serv1Service.selectedUrl);
-    console.log('this.serv.servSearchValue = ' + this.serv1Service.searchUrl);
-    console.log('this.serv.currentVideoUrl = ' + this.serv1Service.currentVideoUrl);
-
     if (from === 'search' && this.serv1Service.searchUrl != null){
       if (this.serv1Service.getEmbedURL(this.serv1Service.searchUrl) != null) {
         document.getElementById('video').setAttribute('src', this.serv1Service.getEmbedURL(this.serv1Service.searchUrl));
@@ -30,10 +24,7 @@ export class VideoViewComponent implements OnInit {
     if (from === 'select' && this.serv1Service.selectedUrl !== this.serv1Service.oldSelectedUrl){
       document.getElementById('video').setAttribute('src', this.serv1Service.getEmbedURL(this.serv1Service.selectedUrl));
     }
-
-    console.log(document.getElementById('video'));
   }
-
 
   ngOnInit(): void {
   }
